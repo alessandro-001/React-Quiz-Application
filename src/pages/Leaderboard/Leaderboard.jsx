06e09f93573badb99f1ea3cfe4adb8952016d2a5
alessandro-001 <alessandrofrondini@gmail.com>
2023-08-28 10:42:46 +0100

    fixes

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import db from '../../../firebase.js';
 import { collection, onSnapshot } from 'firebase/firestore';
 import { Button } from '@mui/material';
-
+import './Leaderboard.css';
 
 export default function Leaderboard() {
 
@@ -13,29 +13,30 @@ export default function Leaderboard() {
       setLeaderboard(snapshot.docs.map((doc) => doc.data()))
       console.log(snapshot.docs);
     })
-  })
+  }, []) // Ensure useEffect runs only once on component mount
 
   return (
-    <>
+    <div className="leaderboard-container">
       <h1>Leaderboard:</h1>
-      {leaderboard && leaderboard.map((board) => {
-        return (
-          <>
+      <div className="scrollable-content">
+        {leaderboard && leaderboard.map((board, index) => (
+          <div key={index} className="entry">
             <p>{board.username}</p>
             <p>{board.category}</p>
             <p>{board.score}</p>
             <p>{board.difficulty}</p>
-          </>
-
-        )
-      })}
+          </div>
+        ))}
+      </div>
       <Button
         variant="contained"
         color="secondary"
         size="large"
-        style={{ alignSelf: "center", marginTop: 20 }}
+        className="back-button"
         href="/"
-      >Back to home</Button>
-    </>
-  )
+      >
+        Back to home
+      </Button>
+    </div>
+  );
 }
